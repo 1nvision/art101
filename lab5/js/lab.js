51789241ea5100eb89c252f0852cd784ed47e5de
Collin Savall <collinsavall10@gmail.com>
2025-04-24 09:15:29 -0700

    lab 5 js and index edit

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Data Types and Variables
+// Author: Collin Savall
+// Date: 4/24/2025
 
 // Constants
-
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
 // Functions
+
+age = 2019 - year;
+
+
+//output
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
