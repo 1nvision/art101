2a735304f11914e7eaa5dd8c65e2c92e409e53c6
Collin Savall <collinsavall10@gmail.com>
2025-04-28 14:20:51 -0700

    Lab 6 editing

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - JavaScript and Arrays
+// Author: Collin Savall
+// Date: 4/28/2025
 
 // Constants
+myTransport = ["Toyota CH-R", " Zip Car", " walking", " Bus", " Bike"]
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+myMainRide = {
+  make: "Toyota",
+  model: "C-HR",
+  color: "Black",
+  year: 2018,
+//function for the age of the car
+  age: function() {
+      return 2025 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//output of code
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
 
-// let's get this party started
-main();
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
